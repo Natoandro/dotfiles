dd240c10d2ce75e79a976615885f874d34ee0959
Natoandro <anatoandro@hotmail.com>
2023-02-26 20:27:37 +0300

    fix bash prompt

--- ps1.sh
+++ ps1.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-
 this="$(dirname "$(realpath "$BASH_SOURCE")")"
 
+# https://github.com/git/git/blob/dadc8e6dacb629f46aee39bde90b6f09b73722eb/contrib/completion/git-prompt.sh
 . $this/vendor/git-prompt.sh
 
 __my_git_ps1 () {
-	echo '$(\
-		GIT_PS1_SHOWDIRTYSTATE=1 \
-		GIT_PS1_SHOWUNTRACKEDFILES=1 \
-		GIT_PS1_SHOWUPSTREAM="verbose,name" \
-		GIT_PS1_SHOWCONFLICTSTATE=1 \
-		GIT_PS1_SHOWCOLORHINTS=1 \
-		__git_ps1 " [%s]")'
+	GIT_PS1_SHOWDIRTYSTATE=1 \
+	GIT_PS1_SHOWUNTRACKEDFILES=1 \
+	GIT_PS1_SHOWUPSTREAM="verbose,name" \
+	GIT_PS1_SHOWCONFLICTSTATE=1 \
+	GIT_PS1_SHOWCOLORHINTS=1 \
+	__git_ps1 " [%s]"
 }
