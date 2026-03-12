112c0d5606035109347447a607f275439d0ebcc3
Natoandro <anatoandro@hotmail.com>
2026-03-12 21:16:53 +0300

    feat(gitsigns): enable inline current line blame (virt text)

--- nvim/lua/plugins/vcs.lua
+++ nvim/lua/plugins/vcs.lua
@@ -4,7 +4,19 @@ return {
         dependencies = {
             "nvim-lua/plenary.nvim",
         },
-        config = true,
+        config = function()
+            require('gitsigns').setup({
+                current_line_blame = true,
+                current_line_blame_opts = {
+                    virt_text = true,
+                    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+                    delay = 1000,
+                    ignore_whitespace = false,
+                },
+                -- a concise default formatter; tweak as desired
+                current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+            })
+        end,
     },
 
 	{
